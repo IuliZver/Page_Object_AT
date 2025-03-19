be54476c964576fb0a6e7c8f0101cec6ce081714
IuliZver <zvereva.iuliia@hotmail.com>
2025-03-19 22:24:23 +0300

    implemented redirection from the main page to the login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,17 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 class MainPage(BasePage):    
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click() 
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        #return LoginPage(browser=self.browser, url=self.browser.current_url)
+        
     def should_be_login_link(self):
-        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
     
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -17,6 +21,6 @@ class MainPage(BasePage):
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except ("NoSuchElementException"):
+        except NoSuchElementException:
             return False
         return True
