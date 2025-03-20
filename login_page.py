62af60d513bdc9c87ed78fe7c079961737d762bf
IuliZver <zvereva.iuliia@hotmail.com>
2025-03-20 15:11:50 +0300

    implemented product page checks

---
+++ login_page.py
@@ -0,0 +1,27 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+        
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "Can't find 'login' in URL"
+        
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+        
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
